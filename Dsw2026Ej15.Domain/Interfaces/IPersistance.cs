05980d20d197811e0c5063e4c9680afdc6631286
Lisandro Ruiz <lisandroruiz05@gmail.com>
2026-06-19 19:52:21 -0300

    Domain completado

---
+++ Dsw2026Ej15.Domain/Interfaces/IPersistance.cs
@@ -0,0 +1,20 @@
+﻿using Dsw2026Ej15.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej15.Domain.Interfaces
+{
+    public interface IPersistance
+    {
+        IEnumerable<Speciality> GetSpecialities();
+        Speciality? GetSpecialityById(Guid id);
+
+        IEnumerable<Doctor> GetActiveDoctors();
+        Doctor? GetActiveDoctorById(Guid id);
+
+        void AddDoctor(Doctor doctor);
+        void UpdateDoctor(Doctor doctor);
+    }
+}
+
