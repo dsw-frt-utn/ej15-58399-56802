1ab398c4a5fca6b78298c6ff6bd9fbc290561c63
Garcia Luca <Luca.Garcia@alu.frt.utn.edu.ar>
2026-07-03 22:21:38 -0300

    cambios realizados por completo

--- Dsw2026Ej15.Api/Program.cs
+++ Dsw2026Ej15.Api/Program.cs
@@ -1,6 +1,8 @@
 using Dsw2026Ej15.Api.Middlewares;
 using Dsw2026Ej15.Data;
+using Dsw2026Ej15.Data.Context;
 using Dsw2026Ej15.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,10 +10,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<IPersistance, PersistanceInMemory>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IPersistance, PersistenceEf>();
 builder.Services.AddHealthChecks();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.UseSwagger();
 app.UseSwaggerUI();
